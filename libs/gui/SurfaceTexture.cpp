f114431147e81f71f243d4c7852d422299ed6464
Neti Ravi Kumar <ravineti@codeaurora.org>
2012-03-09 00:22:33 -0500

    surfacetexture: Select texture target based on pixel format
    Adreno is not optimized for GL_TEXTURE_EXTERNAL_OES, making GPU
    utilization (and power), high with GL_TEXTURE_EXTERNAL_OES for
    RGB formats.
    
    Change texture target from GL_EXTERNAL_TEXTURE_OES to dynamically
    select between GL_EXTERNAL_TEXTURE_OES, and GL_TEXTURE_2D based
    on the pixel format.
    
    This change is done to reduce the composition time for 8x55
    
    *Ported from ics
    *ifdef for BOARD_ADRENO_DECIDE_TEXTURE_TARGET
    
    Change-Id: I33c96bf09fbad0f3fe4ca84c796fca78db51704f

--- libs/gui/SurfaceTexture.cpp
+++ libs/gui/SurfaceTexture.cpp
@@ -195,7 +195,12 @@ status_t SurfaceTexture::releaseBufferLocked(int buf, EGLDisplay display,
     return err;
 }
 
+
+#ifdef DECIDE_TEXTURE_TARGET
+status_t SurfaceTexture::updateTexImage(BufferRejecter* rejecter, bool skipSync, bool isComposition) {
+#else
 status_t SurfaceTexture::updateTexImage(BufferRejecter* rejecter, bool skipSync) {
+#endif
     ATRACE_CALL();
     ST_LOGV("updateTexImage");
     Mutex::Autolock lock(mMutex);
@@ -247,6 +252,28 @@ status_t SurfaceTexture::updateTexImage(BufferRejecter* rejecter, bool skipSync)
             glBindTexture(mTexTarget, mTexName);
             return NO_ERROR;
         }
+
+#ifdef DECIDE_TEXTURE_TARGET
+        // GPU is not efficient in handling GL_TEXTURE_EXTERNAL_OES
+        // texture target. Depending on the image format, decide,
+        // the texture target to be used
+        if(isComposition){
+            switch (mSlots[buf].mGraphicBuffer->format) {
+                case HAL_PIXEL_FORMAT_RGBA_8888:
+                case HAL_PIXEL_FORMAT_RGBX_8888:
+                case HAL_PIXEL_FORMAT_RGB_888:
+                case HAL_PIXEL_FORMAT_RGB_565:
+                case HAL_PIXEL_FORMAT_BGRA_8888:
+                case HAL_PIXEL_FORMAT_RGBA_5551:
+                case HAL_PIXEL_FORMAT_RGBA_4444:
+                    mTexTarget = GL_TEXTURE_2D;
+                    break;
+                default:
+                    mTexTarget = GL_TEXTURE_EXTERNAL_OES;
+                    break;
+            }
+        }
+#endif
 
         GLint error;
         while ((error = glGetError()) != GL_NO_ERROR) {
