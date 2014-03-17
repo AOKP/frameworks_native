0cde6ac39fdb0f6b9840c5215018e8570086fda4
Robert Rozic <rooobertinhooo@gmail.com>
2014-03-17 19:52:34 +0100

    STE Multimedia Fix [2/3]

--- services/surfaceflinger/DisplayDevice.h
+++ services/surfaceflinger/DisplayDevice.h
@@ -170,7 +170,9 @@ private:
     wp<IBinder> mDisplayToken;
 
     // ANativeWindow this display is rendering into
+#ifndef STE_HARDWARE
     sp<ANativeWindow> mNativeWindow;
+#endif
     sp<DisplaySurface> mDisplaySurface;
 
     EGLDisplay      mDisplay;
