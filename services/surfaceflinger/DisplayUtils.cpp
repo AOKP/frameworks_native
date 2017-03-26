548656237cb78d90ceab9597a7973dffacdf8ea5
Andres Pascual <apascual89@gmail.com>
2017-03-26 16:48:37 +0100

    Revert "sf: fixes for HWC2"
    This reverts commit bf07089be910c501eb7e2f8e5bddd107cfbf280e.
    
    Change-Id: I3c4bc61201f658b89c8f30f3eacc582686c2de35

--- services/surfaceflinger/DisplayUtils.cpp
+++ services/surfaceflinger/DisplayUtils.cpp
@@ -90,28 +90,21 @@ Layer* DisplayUtils::getLayerInstance(SurfaceFlinger* flinger,
     return new Layer(flinger, client, name, w, h, flags);
 }
 
-#ifndef USE_HWC2
 HWComposer* DisplayUtils::getHWCInstance(
                         const sp<SurfaceFlinger>& flinger,
                         HWComposer::EventHandler& handler) {
-#ifdef QTI_BSP
+#if defined(QTI_BSP) && !defined(USE_HWC2)
     if(sUseExtendedImpls) {
         return new ExHWComposer(flinger, handler);
     }
 #endif
-    return new HWComposer(flinger,handler);
-}
-#else
-HWComposer* DisplayUtils::getHWCInstance(
-                        const sp<SurfaceFlinger>& flinger) {
-#ifdef QTI_BSP
-    if(sUseExtendedImpls) {
-        return new ExHWComposer(flinger);
-    }
-#endif
+#if defined(USE_HWC2)
+    (void)handler;
     return new HWComposer(flinger);
-}
+#else
+    return new HWComposer(flinger, handler);
 #endif
+}
 
 void DisplayUtils::initVDSInstance(HWComposer* hwc, int32_t hwcDisplayId,
         sp<IGraphicBufferProducer> currentStateSurface, sp<DisplaySurface> &dispSurface,
